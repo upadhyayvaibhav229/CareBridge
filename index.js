85c6eb018a444b57c6c7e8fcb7036c2828d1d422
unknown <upadhyayvaibhav229@gmail.com>
2025-07-23 11:31:15 +0530

    commited

---
+++ index.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    const menu = document.getElementById("menu");
+    const sidebar = document.getElementById("sidebar");
+    menu.classList.toggle("fa-times");
+    sidebar.classList.toggle("open");
+}
+
+
